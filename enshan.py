9a35af9535a68dc6a5602f581a0858b5894f407d
kinshupoon <122308733+kinshupoon@users.noreply.github.com>
2024-01-24 13:51:33 +0800

    Update enshan.py

--- enshan.py
+++ enshan.py
@@ -1,40 +1,28 @@
-import os
-import requests
-import re
+import requests,re,os
 
-# 从replit的secrets中读取值
+#配置恩山的cookie 到配置文件config.sh export enshanck='' 需要推送配置推送加token export plustoken=''
 enshanck = os.environ.get('ENSHANCK')
+#推送加 token
 plustoken = os.environ.get('PUSHPLUS_TOKEN')
-def Push(contents):
-  # 推送加
-  headers = {'Content-Type': 'application/json'}
-  json_data = {
-      "token": plustoken,
-      'title': '恩山签到',
-      'content': contents.replace('\n', '<br>'),
-      "template": "json"
-  }
-  resp = requests.post('http://www.pushplus.plus/send',
-                       json=json_data,
-                       headers=headers).json()
-  print('push+推送成功' if resp['code'] == 200 else 'push+推送失败')
 
+def Push(contents):
+    # 推送加
+    headers = {'Content-Type': 'application/json'}
+    json = {"token": plustoken, 'title': '恩山签到', 'content': contents.replace('\n', '<br>'), "template": "json"}
+    resp = requests.post(f'http://www.pushplus.plus/send', json=json, headers=headers).json()
+    print('push+推送成功' if resp['code'] == 200 else 'push+推送失败')
 
 headers = {
-    "User-Agent":
-    "Mozilla/5.0 (Windows NT 6.3; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/108.0.5359.125 Safari/537.36",
+    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/109.0.0.0 Safari/537.36",
     "Cookie": enshanck,
 }
-
 session = requests.session()
-response = session.get(
-    'https://www.right.com.cn/FORUM/home.php?mod=spacecp&ac=credit&showcredit=1',
-    headers=headers)
+response = session.get('https://www.right.com.cn/FORUM/home.php?mod=spacecp&ac=credit&showcredit=1', headers=headers)
 try:
-  coin = re.findall("恩山币: </em>(.*?)nb &nbsp;", response.text)[0]
-  point = re.findall("<em>积分: </em>(.*?)<span", response.text)[0]
-  res = f"恩山币：{coin}\n积分：{point}"
-  print(res)
-  Push(contents=res)
+    coin = re.findall("恩山币: </em>(.*?)&nbsp;", response.text)[0]
+    point = re.findall("<em>积分: </em>(.*?)<span", response.text)[0]
+    res = f"恩山币：{coin}\n积分：{point}"
+    print(res)
+    Push(contents=res)
 except Exception as e:
-  res = str(e)
+    res = str(e)
